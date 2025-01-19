0ce0b55fbdd187877cc03b4e2dd115662048933c
mandip-openai <mandip@openai.com>
2025-01-19 14:24:54 -0800

    Create init_pkg_jupyter.sh

---
+++ init_pkg_jupyter.sh
@@ -0,0 +1,4 @@
+oaipkg installoai maraschino_rater maraschino_data_pipeline
+
+
+jupyter notebook --allow-root --NotebookApp.token= --no-browser --ip="localhost" --port 8888 --NotebookApp.allow_origin='https://colab.research.google.com' --NotebookApp.port_retries=0 --NotebookApp.disable_check_xsrf=True
